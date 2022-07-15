7639132995fe81f2d26616c2e4f6afc81cd2030e
Luiz Augusto <luizaugustodeandrades@gmail.com>
2022-07-15 09:17:19 -0300

    Atualização do projeto notas dos alunos em java

--- src/principal/Principal_Aluno.java
+++ src/principal/Principal_Aluno.java
@@ -25,14 +25,14 @@ public class Principal_Aluno {
 				System.out.print("DIGITE O NOME DO ALUNO: ");
 				sc.nextLine();
 				String nome = sc.nextLine();
-				
+
 				System.out.println("----------------------------------------------");
 				System.out.println("*** NOTAS DO SEMESTRE 1 ***");
 				System.out.print("DIGITE A 1ª NOTA DO 1º SEMESTRE: ");
 				double nota1_semestre_1 = sc.nextDouble();
 				System.out.print("DIGITE A 2ª NOTA DO 1º SEMESTRE: ");
 				double nota2_semestre_1 = sc.nextDouble();
-				
+
 				System.out.println("----------------------------------------------");
 				System.out.println("*** NOTAS DO SEMESTRE 2 ***");
 				System.out.print("DIGITE A 1ª NOTA DO 2º SEMESTRE: ");
@@ -42,19 +42,37 @@ public class Principal_Aluno {
 
 				vet[i] = new Aluno(nome, nota1_semestre_1, nota2_semestre_1, nota1_semestre_2, nota2_semestre_2);
 			}
-			
+
 			System.out.println("--------------------------------------------------");
 			System.out.println("*** MÉDIA DO 1º SEMESTRE ***");
-			
-			for(int i = 0; i < num; i++) {
+
+			for (int i = 0; i < num; i++) {
 				double mediaSemestre1 = (vet[i].getNota1_semestre_1() + vet[i].getNota2_semestre_1()) / 2;
-				
-				if(mediaSemestre1 >= 7.0) {
+				if (mediaSemestre1 >= 7.0) {
 					String nome = vet[i].getNome().toUpperCase();
-					System.out.printf("%s, VOCÊ ESTÁ APROVADO COM MÉDIA: %.1f%n", nome, mediaSemestre1);
+					System.out.printf("%s, VOCÊ ESTÁ APROVADO NO SEMESTRE 1 COM MÉDIA: %.1f%n", nome, mediaSemestre1);
+				} else {
+					String nome = vet[i].getNome().toUpperCase();
+					System.out.printf("%s, VOCÊ FICOU NA RECUPERAÇÃO DO SEMESTRE 1 COM MÉDIA: %.1f%n", nome, mediaSemestre1);
 				}
 			}
-
+			
+			System.out.println("--------------------------------------------------");
+			System.out.println("*** MÉDIA DO 2º SEMESTRE ***");
+			
+			for (int i = 0; i < num; i++) {
+				double mediaSemestre2 = (vet[i].getNota1_semestre_2() + vet[i].getNota2_semestre_2()) / 2;
+				if (mediaSemestre2 >= 7.0) {
+					String nome = vet[i].getNome().toUpperCase();
+					System.out.printf("%s, VOCÊ ESTÁ APROVADO NO SEMESTRE 2 COM MÉDIA: %.1f%n", nome, mediaSemestre2);
+				}else {
+					String nome = vet[i].getNome().toUpperCase();
+					System.out.printf("%s, VOCÊ FICOU NA RECUPERAÇÃO DO SEMESTRE 2 COM MÉDIA: %.1f%n", nome, mediaSemestre2);
+				}
+			}
+			
+			
+			System.out.println();
 			System.out.print("DESEJA REINICIAR O SISTEMA: ");
 			resp = sc.next().charAt(0);
 		} while (resp == 'S');
