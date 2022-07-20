c14bc7b77ec5747b356561ffeecd54d1ef79e31c
Luiz Augusto <luizaugustodeandrades@gmail.com>
2022-07-20 10:29:25 -0300

    Modificação do Titulo da pagina inicial

--- src/entidades/Aluno.java
+++ src/entidades/Aluno.java
@@ -1,76 +1,7 @@
 package entidades;
 
 public class Aluno {
-	
-	private String nome;
-	private double nota1_semestre_1;
-	private double nota2_semestre_1;
-	private double nota1_semestre_2;
-	private double nota2_semestre_2;
-	
-	
-	public Aluno() {
-	}
-	
-	public Aluno(String nome, double nota1_semestre_1, double nota2_semestre_1, double nota1_semestre_2, double nota2_semestre_2) {
-		this.nome = nome;
-		this.nota1_semestre_1 = nota1_semestre_1;
-		this.nota2_semestre_1 = nota2_semestre_1;
-		this.nota1_semestre_2 = nota1_semestre_2;
-		this.nota2_semestre_2 = nota2_semestre_2;
-		
-	}
 
-	public String getNome() {
-		return nome;
-	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public double getNota1_semestre_1() {
-		return nota1_semestre_1;
-	}
-
-	public void setNota1_semestre_1(double nota1_semestre_1) {
-		this.nota1_semestre_1 = nota1_semestre_1;
-	}
-
-	public double getNota2_semestre_1() {
-		return nota2_semestre_1;
-	}
-
-	public void setNota2_semestre_1(double nota2_semestre_1) {
-		this.nota2_semestre_1 = nota2_semestre_1;
-	}
-
-	public double getNota1_semestre_2() {
-		return nota1_semestre_2;
-	}
-
-	public void setNota1_semestre_2(double nota1_semestre_2) {
-		this.nota1_semestre_2 = nota1_semestre_2;
-	}
-
-	public double getNota2_semestre_2() {
-		return nota2_semestre_2;
-	}
-
-	public void setNota2_semestre_2(double nota2_semestre_2) {
-		this.nota2_semestre_2 = nota2_semestre_2;
-	}
-	
-	public double mediaSemestre1() {
-		return (nota1_semestre_1 + nota2_semestre_1) / 2;
-	}
-	
-	public double mediaSemestre2() {
-		return (nota1_semestre_2  + nota2_semestre_2) / 2;
-	}
-	
-	public double mediaFinal() {
-		return (mediaSemestre1() + mediaSemestre2()) / 2;
-	}
 
 }
--- src/principal/Principal_Aluno.java
+++ src/principal/Principal_Aluno.java
@@ -1,103 +1,28 @@
 package principal;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entidades.Aluno;
-
 public class Principal_Aluno {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
 		Date d1 = new Date(System.currentTimeMillis());
-
 		char resp;
-
-		System.out.print("QUANTOS ALUNOS E NOTAS VÃO SER REGISTRADOS: ");
-		int num = sc.nextInt();
-
-		Aluno[] vet = new Aluno[num];
-
+		
 		do {
-			for (int i = 0; i < num; i++) {
-				System.out.println();
-				System.out.println("*** INFORMAÇÕES DOS ALUNOS ***");
-				System.out.print("DIGITE O NOME DO ALUNO: ");
-				sc.nextLine();
-				String nome = sc.nextLine();
 
-				System.out.println("----------------------------------------------");
-				System.out.println("*** NOTAS DO SEMESTRE 1 ***");
-				System.out.print("DIGITE A 1ª NOTA DO 1º SEMESTRE: ");
-				double nota1_semestre_1 = sc.nextDouble();
-				System.out.print("DIGITE A 2ª NOTA DO 1º SEMESTRE: ");
-				double nota2_semestre_1 = sc.nextDouble();
-
-				System.out.println("----------------------------------------------");
-				System.out.println("*** NOTAS DO SEMESTRE 2 ***");
-				System.out.print("DIGITE A 1ª NOTA DO 2º SEMESTRE: ");
-				double nota1_semestre_2 = sc.nextDouble();
-				System.out.print("DIGITE A 2ª NOTA DO 2º SEMESTRE: ");
-				double nota2_semestre_2 = sc.nextDouble();
-
-				vet[i] = new Aluno(nome, nota1_semestre_1, nota2_semestre_1, nota1_semestre_2, nota2_semestre_2);
-			}
-
-			System.out.println("--------------------------------------------------");
-			System.out.println("*** MÉDIA DO 1º SEMESTRE ***");
-
-			for (int i = 0; i < num; i++) {
-				if (vet[i].mediaSemestre1() >= 7.0) {
-					String nome = vet[i].getNome().toUpperCase();
-					System.out.printf("%s, VOCÊ ESTÁ APROVADO NO SEMESTRE 1 COM MÉDIA: %.1f%n", nome,
-							vet[i].mediaSemestre1());
-				} else {
-					String nome = vet[i].getNome().toUpperCase();
-					System.out.printf("%s, VOCÊ FICOU NA RECUPERAÇÃO DO SEMESTRE 1 COM MÉDIA: %.1f%n", nome,
-							vet[i].mediaSemestre1());
-				}
-			}
-
-			System.out.println("--------------------------------------------------");
-			System.out.println("*** MÉDIA DO 2º SEMESTRE ***");
-
-			for (int i = 0; i < num; i++) {
-				if (vet[i].mediaSemestre2() >= 7.0) {
-					String nome = vet[i].getNome().toUpperCase();
-					System.out.printf("%s, VOCÊ ESTÁ APROVADO NO SEMESTRE 2 COM MÉDIA: %.1f%n", nome,
-							vet[i].mediaSemestre2());
-				} else {
-					String nome = vet[i].getNome().toUpperCase();
-					System.out.printf("%s, VOCÊ FICOU NA RECUPERAÇÃO DO SEMESTRE 2 COM MÉDIA: %.1f%n", nome,
-							vet[i].mediaSemestre2());
-				}
-			}
-
-			System.out.println("--------------------------------------------------");
-			System.out.println("*** MÉDIA FINAL ***");
-
-			for (int i = 0; i < num; i++) {
-				if (vet[i].mediaFinal() >= 7.0) {
-					String nome = vet[i].getNome().toUpperCase();
-					System.out.printf("%s, VOCÊ ESTÁ APROVADO DE ANO COM MÉDIA: %.1f%n", nome, vet[i].mediaFinal());
-				} else {
-					String nome = vet[i].getNome().toUpperCase();
-					System.out.printf("%s, VOCÊ FICOU NA RECUPERAÇÃO FINAL COM MÉDIA: %.1f%n", nome,
-							vet[i].mediaFinal());
-				}
-			}
-			System.out.println();
+			
 			System.out.println("LIMOEIRO, " + sdf1.format(d1));
-
 			System.out.println();
-			System.out.print("DESEJA REINICIAR O SISTEMA: ");
+			System.out.print("DESEJA INSERIR OUTRO ALUNO COM NOTAS: ");
 			resp = sc.next().charAt(0);
+
 		} while (resp == 'S');
 
 		sc.close();
